1078af035a96ec83168e8b784f4b4c005b3a2dd8
Zhicheng Wang <zcwang@thoughtworks.com>
2017-04-26 10:32:13 +0800

    fix: 方案1：使用内存API进行测试

--- src/app/btdz/_apis/btdz.api.spec.ts
+++ src/app/btdz/_apis/btdz.api.spec.ts
@@ -1,17 +1,34 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
 import { BtdzApi } from './btdz.api';
 import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BtdzInMemoryApiService } from '../mock/btdz-in-memory-api.service';
 
 fdescribe('BtdzApi', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpModule],
+      imports: [
+        HttpModule,
+        InMemoryWebApiModule.forRoot(BtdzInMemoryApiService),
+      ],
       providers: [BtdzApi],
     });
   });
 
-  it('should ...', inject([BtdzApi], (service: BtdzApi) => {
-    expect(service).toBeTruthy();
+  let api: BtdzApi;
+
+  beforeEach(inject([BtdzApi], (_api_: BtdzApi) => {
+    api = _api_;
+  }));
+
+  it('should be injected', () => {
+    expect(api).toBeTruthy();
+  });
+
+  it('should query by person', async(() => {
+    api.queryByPerson().subscribe((value) => {
+      expect(value[0].rankNo).toEqual(1);
+    });
   }));
 });
